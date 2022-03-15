ee0a93dd9c2784577faf53af8f7b488fbeefba24
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-03-15 13:26:04 +0000

    Se creo navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  logoStyle,
+  linksStyle,
+  dropDownsStyle,
+  buttonStyle1,
+  buttonStyle2,
+} from "../../styles/navbar.js";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <div style={logoStyle}>ticketGo</div>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <div style={linksStyle}>Inicio</div>
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={dropDownsStyle}
+              >
+                Categorias
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <Link
+                      to="/"
+                      style={{ textDecoration: "none", color: "#5BB2F5" }}
+                    >
+                      Teatro
+                    </Link>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <Link
+                      to="/"
+                      style={{ textDecoration: "none", color: "#5BB2F5" }}
+                    >
+                      Cine
+                    </Link>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="ml-auto">
+          <Link to="/">
+            <button className="btn mx-1" style={buttonStyle1}>
+              Iniciar Sesión
+            </button>
+          </Link>
+          <Link to="/">
+            <button className="btn" style={buttonStyle2}>
+              Registrarse
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
---
+++ src/front/styles/navbar.js
@@ -0,0 +1,31 @@
+const logoStyle = {
+  fontFamily: "Comfortaa",
+  color: "black",
+};
+const linksStyle = {
+  fontFamily: "Montserrat",
+  marginLeft: "10px",
+  marginTop: "8px",
+  color: "#5BB2F5",
+};
+
+const dropDownsStyle = {
+  fontFamily: "Montserrat",
+  color: "#5BB2F5",
+};
+
+const buttonStyle1 = {
+  backgroundColor: "#f8f9fa",
+  color: "#5BB2F5",
+  borderColor: "#5BB2F5",
+  fontFamily: "Montserrat",
+};
+
+const buttonStyle2 = {
+  backgroundColor: "#5BB2F5",
+  color: "white",
+  border: "none",
+  fontFamily: "Montserrat",
+};
+
+export { logoStyle, linksStyle, dropDownsStyle, buttonStyle1, buttonStyle2 };
