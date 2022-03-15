4a52671796ea6aab78401da57220974e7dcb3a04
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-03-15 13:50:27 +0000

    Se creo footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { logoStyle, linksStyle } from "../../styles/navbar";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <div className="container-fluid" style={{ backgroundColor: "#f8f9fa" }}>
+    <footer className="row row-cols-6 py-5 my-5">
+      <div className="col offset-md-2">
+        <div style={logoStyle}>ticketGo</div>
+      </div>
+      <div className="d-flex" style={{ width: "45px" }}>
+        <div className="vr"></div>
+      </div>
+      <ul className="nav flex-column">
+        <li className="nav-item mb-2">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div style={linksStyle}>Inicio</div>
+          </Link>
+        </li>
+        <li className="nav-item mb-2">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div style={linksStyle}>Contacto</div>
+          </Link>
+        </li>
+      </ul>
+      <div
+        className="col offset-3"
+        style={{ color: "#5BB2F5", fontSize: "12px", paddingTop: "50px" }}
+      >
+        ©2022 ticketGo
+      </div>
+    </footer>
+  </div>
 );
