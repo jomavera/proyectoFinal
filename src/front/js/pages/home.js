9f32f99083b7313131ef9796cbe376ff9d002310
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-03-15 17:27:04 +0000

    Se creo pagina de inicio

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,87 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Box } from "../component/box.js";
+import { Cards } from "../component/cards.js";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const { store, actions } = useContext(Context);
+  const datos = [
+    {
+      titulo: "Evento 1",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus consectetur blandit. Nunc mauris ipsum, rhoncus in dolor nec, posuere dignissim nulla. Nam ac tincidunt.",
+      enlace: "/",
+    },
+    {
+      titulo: "Evento 2",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus consectetur blandit. Nunc mauris ipsum, rhoncus in dolor nec, posuere dignissim nulla. Nam ac tincidunt.",
+      enlace: "/",
+    },
+    {
+      titulo: "Evento 3",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus consectetur blandit. Nunc mauris ipsum, rhoncus in dolor nec, posuere dignissim nulla. Nam ac tincidunt.",
+      enlace: "/",
+    },
+    {
+      titulo: "Evento 4",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus consectetur blandit. Nunc mauris ipsum, rhoncus in dolor nec, posuere dignissim nulla. Nam ac tincidunt.",
+      enlace: "/",
+    },
+    {
+      titulo: "Evento 5",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus consectetur blandit. Nunc mauris ipsum, rhoncus in dolor nec, posuere dignissim nulla. Nam ac tincidunt.",
+      enlace: "/",
+    },
+  ];
+  return (
+    <div className="container-fluid" style={{ backgroundColor: "#e9ecef" }}>
+      <div className="row pt-5">
+        <div className="col-2"></div>
+        <div className="col-4">
+          <h1 style={{ fontFamily: "Montserrat" }}>Que quieres hacer hoy?</h1>
+        </div>
+        <div className="col-5"></div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col"></div>
+        <div className="col text-center">
+          <Box texto="Disfrutar un obra de teatro" enlace="/" />
+        </div>
+        <div className="col text-center ">
+          <Box texto="Ver una película junto amigos" enlace="/" />
+        </div>
+        <div className="col text-center">
+          <Box
+            texto="Ir a un concierto de un artista que me gusta"
+            enlace="/"
+          />
+        </div>
+        <div className="col"></div>
+      </div>
+      <div className="row">
+        <div className="container">
+          <hr></hr>
+        </div>
+      </div>
+      <div className="row pt-5">
+        <div className="col-2"></div>
+        <div className="col-4">
+          <h1 style={{ fontFamily: "Montserrat" }}>Oferta eventos</h1>
+        </div>
+        <div className="col-5"></div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col-8">
+          <Cards cartas={datos} />
+        </div>
+        <div className="col"></div>
+      </div>
+    </div>
+  );
 };
