989d8b2dc0ce3194555fd370b9c68ba872364420
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-03-17 00:04:47 +0000

    Se modifico componente formulario compra para que click en boton envie a pagina de ubicaciones

--- src/front/js/component/cajaCompra.js
+++ src/front/js/component/cajaCompra.js
@@ -1,41 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { buttonStyle2 } from "../../styles/navbar.js";
 import { cajaStyle } from "../../styles/cajaCompra.js";
 export const CajaCompra = (props) => {
+  const params = useParams();
   const fechas = ["03-05-2022", "13-06-2022", "12-03-2022"];
   const horas = ["18h30", "22h00"];
   const fechasOptions = fechas.map((e, ix) => {
-    return <option value={e}>{e}</option>;
+    return (
+      <option value={e} key={ix}>
+        {e}
+      </option>
+    );
   });
   const horasOptions = horas.map((e, ix) => {
-    return <option value={e}>{e}</option>;
+    return (
+      <option value={e} key={ix}>
+        {e}
+      </option>
+    );
   });
   return (
     <form style={cajaStyle} className="rounded">
-      <div class="mb-3">
-        <label className="form-check-label" for="exampleCheck1">
-          Seleccionar fecha
-        </label>
-        <select class="form-select" aria-label="seleccion fecha">
+      <div className="mb-3">
+        <label className="form-check-label">Seleccionar fecha</label>
+        <select className="form-select" aria-label="seleccion fecha">
           {fechasOptions}
         </select>
       </div>
       <div className="mb-3">
-        <label className="form-check-label" for="exampleCheck1">
-          Seleccionar hora
-        </label>
-        <select class="form-select" aria-label="seleccion hora">
+        <label className="form-check-label">Seleccionar hora</label>
+        <select className="form-select" aria-label="seleccion hora">
           {horasOptions}
         </select>
       </div>
-      <div class="mb-3">
-        <label classNAme="form-label">Ingresar número de tickets</label>
+      <div className="mb-3">
+        <label className="form-label">Ingresar número de tickets</label>
         <input type="number" className="form-control" />
       </div>
-      <button type="submit" className="btn" style={buttonStyle2}>
-        Comprar
-      </button>
+      <div className="btn btn-primary" style={buttonStyle2}>
+        <Link
+          to={`/eventoUbicaciones/${params.theid}`}
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          Comprar
+        </Link>
+      </div>
     </form>
   );
 };
