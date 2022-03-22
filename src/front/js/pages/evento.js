ded4cd7647a6fe11ccc50287eaa9f93c369aa344
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-03-22 14:01:48 +0000

    Se actualiza vistas de home y eventos para que muestren datos de la base de datos

--- src/front/js/pages/evento.js
+++ src/front/js/pages/evento.js
@@ -2,16 +2,47 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { datos } from "../../../datosPrueba.js";
+// import { datos } from "../../../datosPrueba.js";
 import { CajaCompra } from "../component/cajaCompra";
 
 export const Evento = (props) => {
   const { store, actions } = useContext(Context);
-
+  const [datosEvento, setDatos] = useState({});
+  const [datosLocacion, setLoc] = useState({});
   const params = useParams();
-  const datosEvento = datos.filter((e) => {
-    return e.id === parseInt(params.theid);
-  })[0];
+  // const datosEvento = datos.filter((e) => {
+  //   return e.id === parseInt(params.theid);
+  // })[0];
+
+  async function obtenerDatosEventos(theid) {
+    const response = await fetch(
+      `https://3001-jomavera-proyectofinal-f1p84es4rkr.ws-us38.gitpod.io/api/evento/${theid}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let dataEvento = await response.json();
+    setDatos(dataEvento[0]);
+    const responseLoc = await fetch(
+      `https://3001-jomavera-proyectofinal-f1p84es4rkr.ws-us38.gitpod.io/api/locacion_id/${dataEvento[0].locacion_id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let dataLoc = await responseLoc.json();
+    setLoc(dataLoc[0]);
+  }
+
+  useEffect(() => {
+    obtenerDatosEventos(params.theid);
+  }, []);
+
   const actualizarStore = (e) => {
     actions.actualizarPedido(
       params.theid,
@@ -35,7 +66,7 @@ export const Evento = (props) => {
         <div className="col-4 py-4" style={{ fontFamily: "Montserrat" }}>
           <div className="row">
             <div className="fs-4 fw-bold">Teatro:</div>
-            <div className="fs-4">{datosEvento.locacion}</div>
+            <div className="fs-4">{datosLocacion.name}</div>
           </div>
           <div className="row">
             <div className="fs-4 fw-bold">Obra:</div>
