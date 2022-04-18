15d51fed0665781bf73ee51e830013a7d727b2dc
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-04-18 03:51:36 +0000

    Se valida si usuario es administrador a traves de token

--- src/front/js/pages/ingresarEvento.js
+++ src/front/js/pages/ingresarEvento.js
@@ -1,17 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import PropTypes from "prop-types";
+import PropTypes, { objectOf } from "prop-types";
 import { Context } from "../store/appContext";
 import { FormularioNuevoEvento } from "../component/formularioNuevoEvento";
 
 export const IngresarEvento = (props) => {
   const { store, actions } = useContext(Context);
+  const [admin, setAdmin] = useState(false);
   const history = useHistory();
 
   if (sessionStorage.token === null) {
     history.push("/login");
   }
-  return <FormularioNuevoEvento />;
+
+  async function chequearAdmin() {
+    const opciones = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + store.token,
+      },
+    };
+    try {
+      const resp = await fetch(
+        `${process.env.BACKEND_URL}/api/check_role`,
+        opciones
+      );
+      if (resp.status == 200) {
+        setAdmin(true);
+      }
+    } catch (error) {
+      console.error(`Check admin error: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    chequearAdmin();
+  }, []);
+
+  return (
+    <div>
+      {admin ? (
+        <FormularioNuevoEvento />
+      ) : (
+        <div className="h1 text-center m-5">Usuario no es administrador</div>
+      )}
+    </div>
+  );
 };
 
 IngresarEvento.propTypes = {
