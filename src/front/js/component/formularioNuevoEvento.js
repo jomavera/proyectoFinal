ff5c5016004cdcb8fc78b714e3d15103e395562a
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-04-22 16:08:34 +0000

    Validacion de formulario de ingreso de evento

--- src/front/js/component/formularioNuevoEvento.js
+++ src/front/js/component/formularioNuevoEvento.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import DatePicker from "react-multi-date-picker";
 import TimePicker from "react-multi-date-picker/plugins/time_picker";
 import { buttonStyle2 } from "../../styles/navbar.js";
 
+const initialValues = {
+  nombre: "",
+  descripcion: "",
+  locacion: "",
+  comuna: "",
+  sinopsis: "",
+  duracion: "",
+  precio: "",
+  imagen: "",
+};
+
 export const FormularioNuevoEvento = () => {
   const [fechas, setFechas] = useState(new Date());
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+
   const history = useHistory();
   async function ingresarEventoBase(datos) {
     const response = await fetch(
@@ -36,6 +50,7 @@ export const FormularioNuevoEvento = () => {
   }
   async function manejarSubmit(e) {
     e.preventDefault();
+    if (Object.keys(formErrors).length === 0){
     const fechas_utc = Array.isArray(fechas)
       ? fechas.map((fecha) => {
           return new Date(fecha).toUTCString();
@@ -74,7 +89,48 @@ export const FormularioNuevoEvento = () => {
 
     alert("Evento ingresado exitosamente");
     history.push("/perfil");
+    }
   }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  useEffect(() => {
+    setFormErrors(validate(formValues));
+  }, [formValues]);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.nombre) {
+      errors.nombre = "Se requiere nombre del evento";
+    }
+    if (!values.locacion) {
+      errors.locacion = "Se requiere locacion del evento";
+    }
+    if (!values.comuna) {
+      errors.comuna = "Se requiere comuna del evento";
+    }
+    if (!values.descripcion) {
+      errors.descripcion = "Se requiere descripcion del evento";
+    }
+    if (!values.sinopsis) {
+      errors.sinopsis = "Se requiere sinopsis del evento";
+    }
+    if (!values.duracion) {
+      errors.duracion = "Se requiere duracion del evento";
+    }
+    if (!values.precio) {
+      errors.precio = "Se requiere precio del evento";
+    }
+    if (!values.imagen) {
+      errors.imagen = "Se requiere imagen del evento";
+    }
+
+    return errors;
+  };
+
   return (
     <div className="container">
       <div className="row justify-content-center m-2">
@@ -93,13 +149,16 @@ export const FormularioNuevoEvento = () => {
               className="form-control"
               placeholder="Nombre del evento"
               name="nombre"
+              onChange={handleChange}
             ></input>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.nombre}</div>
             <div className="form-check-label">Ingresar categoria</div>
             <select
               className="form-select"
               aria-label="Default select example"
               defaultValue={1}
               name="categoria_id"
+              onChange={handleChange}
             >
               <option value="1">Teatro</option>
               <option value="2">Cine</option>
@@ -111,13 +170,16 @@ export const FormularioNuevoEvento = () => {
               className="form-control"
               placeholder="Nombre de la locacion"
               name="locacion"
+              onChange={handleChange}
             ></input>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.locacion}</div>
             <div className="form-check-label">Ingresar Comuna</div>
             <input
               type="text"
               className="form-control"
               placeholder="Nombre de comuna"
               name="comuna"
+              onChange={handleChange}
             ></input>
             <div className="form-check-label">Seleccionar funciones</div>
             <DatePicker
@@ -132,20 +194,26 @@ export const FormularioNuevoEvento = () => {
               className="form-control"
               id="floatingTextarea1"
               name="descripcion"
+              onChange={handleChange}
             ></textarea>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.descripcion}</div>
             <div className="form-check-label">Ingresar sinopsis</div>
             <textarea
               className="form-control"
               id="floatingTextarea2"
               name="sinopsis"
+              onChange={handleChange}
             ></textarea>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.sinopsis}</div>
             <div className="form-check-label">Ingresar precio</div>
             <input
               type="number"
               min={1000}
               className="form-control"
               name="precio"
+              onChange={handleChange}
             ></input>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.precio}</div>
             <div className="form-check-label">Duracion</div>
             <input
               type="number"
@@ -153,9 +221,17 @@ export const FormularioNuevoEvento = () => {
               className="form-control"
               placeholder="Duracion evento en horas"
               name="duracion"
+              onChange={handleChange}
             ></input>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.duracion}</div>
             <div className="form-check-label">Ingresar URL de imagen</div>
-            <input type="text" className="form-control" name="imagen"></input>
+            <input
+              type="text"
+              className="form-control"
+              name="imagen"
+              onChange={handleChange}
+            ></input>
+            <div className="text-danger" style={{fontSize:"12px"}}>{formErrors.imagen}</div>
             <button
               type="submit"
               className="btn btn-primary m-1"
